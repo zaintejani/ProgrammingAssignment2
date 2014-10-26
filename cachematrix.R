606054ea9dd1e84c3db3fbe44545f97472639d4a
zain.tejani <zain.tejani@gmail.com>
2014-10-26 14:32:21 -0400

    Completed assignemnt with functions to cache and invert a matrix. Updated with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,24 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## The first function takes a symmetrical matrix as the input and outputs a list that is a cache of the matrix
+## The second function takes the first as the input argument, and outputs the inverse of the matrix.
+## The second fuction stores the inverse for future recall, and updates this value in the first function
+## using the <<- operator
+
+
 ## Write a short comment describing this function
 
-## Modified example makeVector function to accomodate matrices instead. 
-## Found a loophole, when calling a$setinv(inv) or a$setmean(mean) in the original function, you can
-## basically input any value and the cache will print with no error. For example, I set the inverse of
-## a 2X2 matrix to a 3X3 matrix using a$setinv(inv). Running cacheSolve(a) printed the 3X3 with no error.
+## Modified example makeVector function to accomodate matrices instead.
+## In this function, a symmetrical input matrix is manipulated within a list of sub-functions to create a cache.
+## The <<- assignment operator is used to upate the matrix inverse calculated in cacheSolve into makeCacheMatrix.
 
+## Included an if loop to check the matrix dimensions and ensure symmetry in the input data.
+## Will output an error message in case of asymmetrical data.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+if (nrow(x) == ncol(x)){
+  
   m <- NULL
   set <- function(y) {
     x <<- y
@@ -22,13 +30,18 @@ makeCacheMatrix <- function(x = matrix()) {
   list(set = set, get = get,
        setinv = setinv,
        getinv = getinv)
-  
+  } 
+else {message("Invalid matrix argument. Number of rows are not equal to number of columns")}
 }
 
 
 ## Write a short comment describing this function
 
 ## Modified exaple cacheMean function using the solve function instead, to accomodate matrix input.
+## This function returns the inverse matrix of the original argument, and takes the makeCacheMatrix function
+## as the input argument. The use of the solve functionis minimized by the use of the if loop. This saves
+## time as the solve function is relatively complicated.
+
 
 cacheSolve <- function(x, ...) {    
   
@@ -46,6 +59,10 @@ cacheSolve <- function(x, ...) {
   
   x$setinv(m)
   
-  m
+  ## Found a loophole, when calling a$setinv(inv), you can basically input any value and the cache
+  ## will print with no error. For example, I set the inverse of a 2X2 matrix to a 3X3 matrix using
+  ## a$setinv(inv). Running cacheSolve(a) printed the 3X3 with no error.
   
+  m
+
 }
